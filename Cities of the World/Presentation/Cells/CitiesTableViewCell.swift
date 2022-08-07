f010a432eca9500e0cf0ea31637aad75452074ef
usman.kulaha <usman.kulaha@blinqpay.io>
2022-08-07 08:21:32 +0100

    added Search bar and tableview to main storyboard created CitiesTableViewCell

---
+++ Cities of the World/Presentation/Cells/CitiesTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CitiesTableViewCell.swift
+//  Cities of the World
+//
+//  Created by Usman.Kulaha on 07/08/2022.
+//
+
+import UIKit
+
+class CitiesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
